19117961cd7e4717d9644af81a68b56eb2115227
Akhigbesimeon <s.akhigbe@alustudent.com>
2025-09-14 00:23:26 +0200

    3-print_alphabets.c task completed

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - Print the alphabet in lowercase, followed by a new line.
+ * main - Print the alphabet in lowercase, and then in upper case
+ * followed by a new line.
  *
  * Return: Always 0 (Success)
  */
@@ -10,6 +11,8 @@ int main(void)
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	for (letter = 'A'; letter <= 'Z'; letter++)
 		putchar(letter);
 	putchar('\n');
 
